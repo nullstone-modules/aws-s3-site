b7a3bc4288630e42f62441bd6369fe2aa5ee6381
Brad Sickles <brad.sickles@gmail.com>
2022-02-10 14:02:33 -0500

    Fixed module for upgrade aws tf provider

--- bucket.tf
+++ bucket.tf
@@ -1,12 +1,23 @@
 resource "aws_s3_bucket" "this" {
   bucket        = local.resource_name
-  acl           = "private"
   tags          = local.tags
   force_destroy = true
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
   }
 }
 
