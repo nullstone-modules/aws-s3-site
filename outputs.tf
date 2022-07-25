3da18a4d55ba9d3f8b3925733614564c55cfb1b3
Brad Sickles <brad.sickles@gmail.com>
2022-07-25 10:49:21 -0400

    Added artifacts outputs to align with app modules with s3 artifacts

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,16 @@ output "bucket_name" {
   description = "string ||| The name of the created S3 bucket."
 }
 
+output "artifacts_bucket_name" {
+  value       = aws_s3_bucket.this.bucket
+  description = "string ||| The name of the created S3 bucket."
+}
+
+output "artifacts_key_template" {
+  value       = "{{app-version}}/"
+  description = "string ||| Template for s3 directory where files are placed."
+}
+
 output "deployer" {
   value = {
     name       = aws_iam_user.deployer.name
