f6c2928fdfbc4e36d31b44696a38e6b4e98f6e01
Brad Sickles <brad.sickles@gmail.com>
2023-01-09 15:14:37 -0500

    Make root artifacts key empty string

--- variables.tf
+++ variables.tf
@@ -25,5 +25,5 @@ variable "enable_versioned_assets" {
 }
 
 locals {
-  artifacts_key_template = var.enable_versioned_assets ? "{{app-version}}/" : "/"
+  artifacts_key_template = var.enable_versioned_assets ? "{{app-version}}/" : ""
 }
