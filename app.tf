3a71c86e870944e1b7818888259e463c6c6f4309
Brad Sickles <brad.sickles@gmail.com>
2023-08-29 16:36:51 -0400

    Fixed initial launch issue where an attached CDN redirects to the S3 bucket URL.

--- app.tf
+++ app.tf
@@ -11,8 +11,11 @@ locals {
 locals {
   app_metadata = tomap({
     // Inject app metadata into capabilities here (e.g. security_group_name, role_name)
-    s3_domain_name         = aws_s3_bucket.this.bucket_domain_name
     s3_bucket_id           = aws_s3_bucket.this.id
     artifacts_key_template = local.artifacts_key_template
+    // AWS recommends using the regional bucket domain name when creating CloudFront distribution origin
+    // https://stackoverflow.com/questions/38735306/aws-cloudfront-redirecting-to-s3-bucket
+    // This prevents issues where initial launch forces a redirect to the S3 bucket
+    s3_domain_name = aws_s3_bucket.this.bucket_regional_domain_name
   })
 }
